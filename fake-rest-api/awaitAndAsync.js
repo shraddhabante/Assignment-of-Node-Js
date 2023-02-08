8152f3523c790d1e6aff1f61e32d87a9fd6b071a
shraddhabante27 <shraddhabante27@gmail.com>
2023-02-08 11:45:04 +0530

    file changes

--- fake-rest-api/awaitAndAsync.js
+++ fake-rest-api/awaitAndAsync.js
@@ -5,8 +5,19 @@ async function storeData() {
         let res = await fetch("https://jsonplaceholder.typicode.com/todos");
         let result = await res.json();
         console.log(result);
-    }catch(e){
-        console.log("error generated "+e)
+    } catch (e) {
+        console.log("error generated " + e)
     }
 }
 storeData();
+
+async function storeProductData() {
+    try {
+        var res = await fetch("https://dummyjson.com/products");
+        var result = await res.json();
+        console.log(result);
+    } catch (e) {
+        console.log("error generated " + e);
+    }
+}
+storeProductData(); 
